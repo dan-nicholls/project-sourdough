d2e662c507944ab1850d5ac56d7e14b3125fefd7
Dan Nicholls <dannicholls12@gmail.com>
2025-06-04 18:16:21 +1000

    feat: Initial styling and set default font
    - Default font is now set to 'Outfit'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./internal/web/templates/*.templ"],
   theme: {
-    extend: {},
+    extend: {
+		colors: {
+			background: "#FFF5E1",
+			text: {
+				light: "#333333",
+				dark: "#FAFAFA",
+			},
+			pink: '#FF6F91',
+			blue: '#4D9DE0',
+			yellow: '#FFE156',
+			green: '#A0E8AF',
+			purple: '#8E6EC2',
+			red: '#FF8C8C',
+		},
+		fontFamily: {
+			sans: ['Outfit', 'sans-serif']
+		}	
+	},
   },
   plugins: [],
 }
